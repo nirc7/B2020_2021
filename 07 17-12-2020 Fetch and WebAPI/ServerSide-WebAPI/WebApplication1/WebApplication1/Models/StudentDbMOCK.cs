95645aaf31000d9bca8e51af2ecd74292ce65f18
nirc7 <i2cs2013@gmail.com>
2020-12-17 14:13:49 +0200

    fetch demo

---
+++ 07 17-12-2020 Fetch and WebAPI/ServerSide-WebAPI/WebApplication1/WebApplication1/Models/StudentDbMOCK.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public static class StudentDbMOCK
+    {
+        public static List<Student> students = new List<Student>() {
+            new Student(1,"avi", 100),
+            new Student(3,"ben", 99),
+            new Student(2,"charlie", 97),
+            new Student(4,"dora", 100),
+        };
+    }
+}
