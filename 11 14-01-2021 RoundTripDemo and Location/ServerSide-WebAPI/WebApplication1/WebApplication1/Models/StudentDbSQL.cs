5efcb856085e9131a4c9c4a59657f853db82cc74
nirc7 <i2cs2013@gmail.com>
2021-01-14 11:29:31 +0200

    round trip

---
+++ 11 14-01-2021 RoundTripDemo and Location/ServerSide-WebAPI/WebApplication1/WebApplication1/Models/StudentDbSQL.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public static class StudentDbSQL
+    {
+        public static List<Student> students = new List<Student>() {
+            new Student(1,"avi", 100),
+            new Student(3,"ben", 99),
+            new Student(2,"charlie", 97),
+            new Student(4,"dora", 100),
+        };
+
+
+        public static List<Student> GetALLStudents()
+        {
+            string conStr = ConfigurationManager.ConnectionStrings["RuppinSqlServer"].ConnectionString;
+            SqlConnection con = new SqlConnection(conStr);
+            SqlCommand comm = new SqlCommand("SELECT * FROM TBStudents", con);
+            con.Open();
+            SqlDataReader reader = comm.ExecuteReader();
+            List<Student> ls = null;
+            if (reader.HasRows)
+            {
+                ls = new List<Student>();
+                while (reader.Read())
+                {
+                    ls.Add(new Student(
+                        (int)reader["ID"],
+                        (string)reader["Name"],
+                        (int)reader["Grade"]));
+                }
+
+            }
+            con.Close();
+            return ls;
+        }
+    }
+}
